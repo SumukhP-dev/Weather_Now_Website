2c3172abf8d2e311991b568d8fa61f4e58d34e72
Sumukh Paspuleti <156015540+SumukhP-dev@users.noreply.github.com>
2025-03-31 05:39:49 -0400

    Add mysql database using settings.py

--- core-api/DataCore/settings.py
+++ core-api/DataCore/settings.py
@@ -10,11 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -83,11 +87,14 @@ WSGI_APPLICATION = 'DataCore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'defaultdb',
+        'USER': 'avnadmin',
+        'PASSWORD': env("DATABASE_PASSWORD"),
+        'HOST': 'mysql-10ab44ad-weather-now.h.aivencloud.com',
+        'PORT': '21477'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
