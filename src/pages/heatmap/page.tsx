b87355462ce5b132a5f8f805860ba2842ff66ae1
Sumukh Paspuleti <sumukh.paspuleti@gmail.com>
2025-08-06 13:31:46 -0400

    Move to use React.js over Next.js

---
+++ src/pages/heatmap/page.tsx
@@ -0,0 +1,10 @@
+import NavBar from "@/react-weather-now-website/src/components/NavBar";
+
+export default function HeatmapPage() {
+  return (
+    <>
+      <NavBar />
+      <h1>Heatmap</h1>
+    </>
+  );
+}
